58a8d22bc0e5b53489345640e465f62d5806cbcd
Ruth <ruth@pobtwinks-imac.lan>
2021-02-27 21:24:57 -0500

    Played around with beautiful soup

--- zeit_connect.py
+++ zeit_connect.py
@@ -1,6 +1,7 @@
 import requests
 import pandas as pd 
 import urllib
+from bs4 import BeautifulSoup
 
 # settings for the general site
 my_token = 'e95117f10bc2a32e551d4bbbcf3e2d78a3d60c4b1421a65aa75e'
@@ -26,3 +27,18 @@ df = pd.DataFrame(response.json()['matches'])
 
 print(f'Your options are: {df.columns}')
 print(df[['title','href','release_date']])
+
+url_lst = df['href'].tolist()[0:10]
+
+# iterate over the url list -- > for every url in the list
+u = url_lst[0]
+
+# retrieve the page
+page = urllib.request.urlopen(u)
+# convert it to soup
+soup = BeautifulSoup(page)
+# find the article div
+t = soup.find("div", {"class":"article-body"})
+# for every paragraph in that div, return the text
+paras = t.findAll('p')
+paras[0].text
